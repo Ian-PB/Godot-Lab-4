a3fbdf5b1456e13b1ffef4678208b19120f23807
(Student  C00294680) Ian Perez Bunuel <C00294680@setu.ie>
2025-03-13 16:43:38 +0000

    Coin started - script not working

---
+++ game/scripts/coin.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("HELLO")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
