058bc4b70d2398c915ee0c100ae1a6302fc2aed1
(Student  C00294680) Ian Perez Bunuel <C00294680@setu.ie>
2025-03-18 12:08:06 +0000

    Added coin and background

--- game/scripts/coin.gd
+++ game/scripts/coin.gd
@@ -1,11 +1,5 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("HELLO")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _on_body_entered(body : Node2D) -> void:
+	print("+1 coin")
+	queue_free()
